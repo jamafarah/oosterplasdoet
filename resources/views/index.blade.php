ccf6d7f9980e78fb3475edfa9756ca90225f7ff9
Farah <testA@MacBook-Pro-van-Farah.local>
2017-04-06 23:28:13 +0200

    missed a div, still more responsive thoug

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -57,7 +57,7 @@
 
             </div>
         </div>
-
+</div>
     </div>
     <div class="banner row-4 orange-background" style="background-image: url(http://www.panoramawinkel.nl/upload/products/1394527120-art-Panoramafoto_Den_Bosch_Oeteldonk_Markt_Oeteldonkse_dinsdag_S_00173-2500.jpg);">
         <h1 class="white">higlights/activiteiten</h1>
