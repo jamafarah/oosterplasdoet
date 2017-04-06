e8d3bf9cceea843b2b842e2adf77d44f147c0b70
Farah <testA@MacBook-Pro-van-Farah.local>
2017-04-06 23:26:10 +0200

    homepage has been updated and more responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,7 +26,10 @@
             </p>
         </div>
         <div class="services">
-            <div class="serviceBlock col-md-6 green-background" style="background-image: url(http://wvua23.com/wp-content/uploads/2015/04/school-stock.jpg)">
+            <div class="serviceBlock col-xs-12 col-md-6">
+                <div class="image" style="background-image: url(http://wvua23.com/wp-content/uploads/2015/04/school-stock.jpg)">
+
+                </div>
                 <div class="blurred contentPane">
                     <h3 class="orange">Huiswerk begeleiding</h3>
                     <p>
@@ -34,10 +37,14 @@
                         Je leert plannen en je krijgt begeleiding om je brugklas jaar goed aan te pakken. Dit alles vindt plaats bij de Biechten en er is tussentijds ook ruimte om even lekker
                         te ontspannen of juist even te bewegen om daarna met hernieuwd concentratie weer verder te gaan.
                     </p>
-                    <a href="" class="button more vague-orange-background">Lees meer</a>
+                    <button class="more orange-background">Lees meer</button>
                 </div>
             </div>
-            <div class="serviceBlock col-md-6 orange-background" style="background-image: url(http://i.telegraph.co.uk/multimedia/archive/03219/stock-photos-vince_3219813k.jpg)">
+
+            <div class="serviceBlock col-xs-12 col-md-6">
+                <div class="image" style="background-image: url(http://i.telegraph.co.uk/multimedia/archive/03219/stock-photos-vince_3219813k.jpg)">
+
+                </div>
                 <div class="blurred contentPane">
                     <h3 class="green">Gunbanen</h3>
                     <p>
@@ -46,35 +53,36 @@
                         Met behoud van hun uitkering, en zonder de sollicitatieplicht.
                         In ruil voor het geleverde werk en getoonde motivatie gaat Oosterplas doet! na een half jaar binnen het netwerk van vijfduizend leden op zoek naar een betaalde baan.”
                     </p>
-                    <a href="" class="button more vague-orange-background">Lees meer</a>
-                </div>
+                    <button class="more orange-background">Lees meer</button>
+
             </div>
         </div>
+
     </div>
     <div class="banner row-4 orange-background" style="background-image: url(http://www.panoramawinkel.nl/upload/products/1394527120-art-Panoramafoto_Den_Bosch_Oeteldonk_Markt_Oeteldonkse_dinsdag_S_00173-2500.jpg);">
         <h1 class="white">higlights/activiteiten</h1>
     </div>
-    <div class="container">
-        <div class="row highlightRow">
-            <div class="col-md-4 row-6 highlightBlock">
-                <div class="row thumbnail" style="background-image: url(http://www.oosterplasdoet.nl/wp-content/uploads/2016/03/FullSizeRender.jpg)"></div>
-                <h3 class="orange title">B-Battle</h3>
-                <h3 class="orange date">6/9/2017</h3>
-                <p>Het is alweer voorbij. We kijken terug op 2 mooie dagen baggerloop en B-Battle. Mooi weer, enthousiaste kinderen en Battle teams. We weten het nu al zeker. Volgend jaar zijn we er weer! Zie meer hier
+<div class="container">
+    <div class="row highlightRow">
+        <div class="col-xs-12 col-md-4 row-6 highlightBlock">
+            <div class="row image thumbnail" style="background-image: url(http://www.oosterplasdoet.nl/wp-content/uploads/2016/03/FullSizeRender.jpg)"></div>
+            <h3 class="orange title">B-Battle</h3>
+            <h3 class="orange date">6/9/2017</h3>
+            <p>Het is alweer voorbij. We kijken terug op 2 mooie dagen baggerloop en B-Battle. Mooi weer, enthousiaste kinderen en Battle teams. We weten het nu al zeker. Volgend jaar zijn we er weer! Zie meer hier
 
-                    Winnaar B-Battle 2016: Zeilvereniging Oosterplas. Gefeliciteerd!</p>
-            </div>
-            <div class="col-md-4 row-6 highlightBlock">
-                <div class="row thumbnail" style="background-image: url(http://www.oosterplasdoet.nl/wp-content/uploads/2016/03/IMG_6991-e1465718742680.jpg)"></div>
-                <h3 class="orange title">BAGGERLOOP 2016</h3>
-                <h3 class="orange date">20/3/2016</h3>
-                <p>Baggerloop is een jaarlijks evenement dat georganiseerd wordt in samenwerking met ‘S-PORT. Basisschool scholieren van diverse scholen lopen op vrijdagochtend een rondje rond de Oosterplas en moeten daarbij allerlei obstakels trotseren.</p>
-            </div>
-            <div class="col-md-4 row-6 highlightBlock">
-                <div class="row thumbnail" style="background-image: url(http://www.oosterplasdoet.nl/wp-content/uploads/2016/03/IMG_6892-e1465718042625.jpg)"></div>
-                <h3 class="orange title">komend evenement</h3>
-                <h3 class="orange date">9/7/17</h3>
-                <p>Dit jaar liepen er weer ongeveer 650 leerlingen mee. Het was opnieuw een groot succes.
+                Winnaar B-Battle 2016: Zeilvereniging Oosterplas. Gefeliciteerd!</p>
+        </div>
+        <div class="col-xs-12 col-md-4 row-6 highlightBlock">
+            <div class="row image thumbnail" style="background-image: url(http://www.oosterplasdoet.nl/wp-content/uploads/2016/03/IMG_6991-e1465718742680.jpg)"></div>
+            <h3 class="orange title">BAGGERLOOP 2016</h3>
+            <h3 class="orange date">20/3/2016</h3>
+            <p>Baggerloop is een jaarlijks evenement dat georganiseerd wordt in samenwerking met ‘S-PORT. Basisschool scholieren van diverse scholen lopen op vrijdagochtend een rondje rond de Oosterplas en moeten daarbij allerlei obstakels trotseren.</p>
+        </div>
+        <div class="col-xs-12 col-md-4 row-6 highlightBlock">
+            <div class="row image thumbnail" style="background-image: url(http://www.oosterplasdoet.nl/wp-content/uploads/2016/03/IMG_6892-e1465718042625.jpg)"></div>
+            <h3 class="orange title">komend evenement</h3>
+            <h3 class="orange date">9/7/17</h3>
+            <p>Dit jaar liepen er weer ongeveer 650 leerlingen mee. Het was opnieuw een groot succes.
 
                     Het weer was mooi en de kinderen enthousiast. Voor meer foto’s kunt u terecht op onze facebook pagina. OosterplasDOET!</p>
             </div>
@@ -85,7 +93,7 @@
     </div>
     <div class="container social-contact">
         <div class="row">
-            <div class="col-md-6 row-5 text-center socialmedia-block">
+            <div class="col-xs-12 col-md-6 row-5 text-center socialmedia-block">
                 <div>
                     <a href="https://www.facebook.com/OosterplasDoet">
                         <i class="fa fa-5x fa-facebook"></i>
@@ -96,7 +104,7 @@
                     <p>Volg ons op op faceboook en twitter om op de hoogte te blijven</p>
                 </div>
             </div>
-            <div class="col-md-6 row-5">
+            <div class="col-xs-12 col-md-6 row-5">
                 <p>
                     Wij zoeken graag naar feedback voor onze site en organiatie. <br>
                     Heb jij nog feedback of een idee voor ons? Laat dan hier achter. <br>
