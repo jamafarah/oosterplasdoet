0e4c1c16acac1d0fb7c53c0df1eee3064c66cbe9
JulesAvans <jfmtakac@avans.nl>
2017-05-18 19:19:46 +0200

    Fix navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,10 +3,12 @@
             <img src="//www.oosterplasdoet.nl/wp-content/uploads/2016/11/Schermafbeelding-2016-11-02-om-22.13.56-e1478121477474.png" alt="" class="logo">
             <ul class="nav navbar-nav">
                 <li><a href="{{ url('/') }}">home</a></li>
+                <li><a href="{{ url('/nieuws') }}">nieuws</a></li>
                 <li><a href="{{ url('/partners') }}">partners</a></li>
+
                 <li class="dropdown">
 
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services <span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">services <span class="caret"></span></a>
 
                     <ul class="dropdown-menu">
                         <li><a href="{{ url('/gunbanen') }}">gunbanen</a></li>
