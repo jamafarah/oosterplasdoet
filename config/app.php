a0c1eb6e8808cfacb2b63ef594c8c06072c08aeb
simonUrselmann <splursel@avans.nl>
2017-05-31 14:12:44 +0200

    afbeelding uploaden

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
@@ -179,6 +180,8 @@ return [
 
 
         Collective\Html\HtmlServiceProvider::class,
+        Unisharp\Laravelfilemanager\LaravelFilemanagerServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
     ],
 
@@ -233,6 +236,7 @@ return [
         'Carbon' => Carbon\Carbon::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -13,28 +13,40 @@
         "insertdatetime media nonbreaking save table contextmenu directionality",
         "emoticons template paste textcolor colorpicker textpattern "
     ],
-            toolbar: "insertfile undo redo | fontsizeselect | forecolor backcolor | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+//            file: {title: 'File', items: 'newdocument'},
+//            edit: {title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall'},
+//            insert: {title: 'Insert', items: 'link media | template hr'},
+//            view: {title: 'View', items: 'visualaid'},
+//            format: {title: 'Format', items: 'bold italic underline strikethrough superscript subscript | formats | removeformat'},
+//            table: {title: 'Table', items: 'inserttable tableprops deletetable | cell row column'},
+//            tools: {title: 'Tools', items: 'spellchecker code'}
+            menubar: "edit,table",
+            toolbar: "undo redo | fontsizeselect | forecolor backcolor | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
             relative_urls: false,
             file_browser_callback : function(field_name, url, type, win) {
-        var x = window.innerWidth || document.documentElement.clientWidth || document.getElementByTagName('body')[0].clientWidth;
-        var y = window.innerHeight|| document.documentElement.clientHeight|| document.grtElementByTagName('body')[0].clientHeight;
-        var cmsURL = editor_config.path_absolute+'laravel-filemanaget?field_name'+field_name;
-        if (type = 'image') {
-            cmsURL = cmsURL+'&type=Images';
-        } else {
-            cmsUrl = cmsURL+'&type=Files';
-        }
+                var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+                var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
 
-        tinyMCE.activeEditor.windowManager.open({
+                var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+                if (type == 'image') {
+                    cmsURL = cmsURL + "&type=Images";
+                } else {
+                    cmsURL = cmsURL + "&type=Files";
+                }
+
+                tinyMCE.activeEditor.windowManager.open({
                     file : cmsURL,
                     title : 'Filemanager',
                     width : x * 0.8,
-                    height : y*0.8 ,
-                    resizeble : 'yes',
-                    close_previous : 'no'
+                    height : y * 0.8,
+                    resizable : "yes",
+                    close_previous : "no"
                 });
             }
         };
 
         tinymce.init(editor_config);
     </script>﻿
+<script>
+    {!! \File::get(base_path('vendor/unisharp/laravel-filemanager/public/js/lfm.js')) !!}
+</script>
